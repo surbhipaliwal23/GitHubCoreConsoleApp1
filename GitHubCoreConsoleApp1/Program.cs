c0d595b26186682860ad022bfa0e07a8f8b68b35
Surbhi PAliwal <surbhipaliwal23@gmail.com>
2022-05-30 12:35:12 +0530

    New line added

---
+++ GitHubCoreConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GitHubCoreConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("New Line Added to main");
+        }
+    }
+}
